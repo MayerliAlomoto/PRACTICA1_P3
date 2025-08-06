16911380afdd567bb6fb6f20676ef6ba82d20613
MayerliAlomoto <maalomoto4@espe.edu.ec>
2025-08-05 19:26:15 -0500

    base de datos

---
+++ project_db_2025-08-05_192446.sql
@@ -0,0 +1,358 @@
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: project_db
+-- ------------------------------------------------------
+-- Server version	8.2.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `article`
+--
+
+DROP TABLE IF EXISTS `article`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `article` (
+  `publication_id` int NOT NULL AUTO_INCREMENT,
+  `doi` varchar(100) NOT NULL,
+  `abstract` varchar(255) NOT NULL,
+  `keywords` varchar(255) NOT NULL,
+  `indexation` varchar(100) NOT NULL,
+  `magazine` varchar(100) NOT NULL,
+  `knownlegde_area` varchar(100) NOT NULL,
+  PRIMARY KEY (`publication_id`),
+  CONSTRAINT `article_ibfk_1` FOREIGN KEY (`publication_id`) REFERENCES `publication` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `article`
+--
+
+/*!40000 ALTER TABLE `article` DISABLE KEYS */;
+INSERT INTO `article` VALUES (2,'10.1000/aihc2024','AI applications in diagnostics','AI, Health','Scopus','Medical Tech Today','Health Sciences'),(4,'10.1000/qcomp2023','Advances in quantum tech','Quantum, Computing','Web of Science','Quantum Journal','Physics'),(5,'10.1000/cybersec2024','Threat detection methods','Security, Networks','IEEE Xplore','CyberSec Weekly','Computer Science');
+/*!40000 ALTER TABLE `article` ENABLE KEYS */;
+
+--
+-- Table structure for table `author`
+--
+
+DROP TABLE IF EXISTS `author`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `author` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `orcid` varchar(20) NOT NULL,
+  `affilation` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `author`
+--
+
+/*!40000 ALTER TABLE `author` DISABLE KEYS */;
+INSERT INTO `author` VALUES (1,'Alice','Johnson','alicej','pass123','alice.johnson@example.com','0000-0001-2345-6789','University of Knowledge'),(2,'Bob','Smith','bobsmith','pass456','bob.smith@example.com','0000-0002-3456-7890','Tech Institute'),(3,'Carol','Lee','caroll','pass789','carol.lee@example.com','0000-0003-4567-8901','Research Labs'),(4,'David','Miller','davidm','passabc','david.miller@example.com','0000-0004-5678-9012','Innovation Center'),(5,'Eve','Brown','eveb','passdef','eve.brown@example.com','0000-0005-6789-0123','Global University');
+/*!40000 ALTER TABLE `author` ENABLE KEYS */;
+
+--
+-- Table structure for table `book`
+--
+
+DROP TABLE IF EXISTS `book`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `book` (
+  `publication_id` int NOT NULL AUTO_INCREMENT,
+  `isbn` varchar(20) NOT NULL,
+  `genre` varchar(100) NOT NULL,
+  `edition` varchar(50) NOT NULL,
+  PRIMARY KEY (`publication_id`),
+  CONSTRAINT `book_ibfk_1` FOREIGN KEY (`publication_id`) REFERENCES `publication` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `book`
+--
+
+/*!40000 ALTER TABLE `book` DISABLE KEYS */;
+INSERT INTO `book` VALUES (1,'978-1-23456-789-7','Technology','1st'),(3,'978-1-98765-432-1','Education','2nd'),(6,'978-3-16-148410-0','Science Fiction','1st Edition');
+/*!40000 ALTER TABLE `book` ENABLE KEYS */;
+
+--
+-- Table structure for table `publication`
+--
+
+DROP TABLE IF EXISTS `publication`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `publication` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `publication_date` date NOT NULL,
+  `author_id` int NOT NULL,
+  `type` enum('book','article') NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `author_id` (`author_id`),
+  CONSTRAINT `publication_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `author` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `publication`
+--
+
+/*!40000 ALTER TABLE `publication` DISABLE KEYS */;
+INSERT INTO `publication` VALUES (1,'Data Science Handbook','Comprehensive guide','2023-01-15',1,'book'),(2,'AI in Healthcare','Study on applications','2024-02-20',2,'article'),(3,'Machine Learning Guide','Introductory book','2022-05-10',3,'book'),(4,'Quantum Computing','Research findings','2023-08-05',4,'article'),(5,'Cybersecurity Trends','Industry analysis','2024-04-12',5,'article'),(6,'The Future of AI','Exploring the implications of AI in society.','2024-05-01',1,'book');
+/*!40000 ALTER TABLE `publication` ENABLE KEYS */;
+
+--
+-- Dumping routines for database 'project_db'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `sp_article_list` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_article_list`()
+BEGIN
+    SELECT
+        a.publication_id,
+        p.title,
+        p.description,
+        p.publication_date,
+        
+        
+        a.doi,
+        a.abstract,
+        a.keywords,
+        a.indexation,
+        a.magazine,
+        a.area,
+
+        
+        auth.id           AS author_id,
+        auth.first_name,
+        auth.last_name,
+        auth.username,
+        auth.email,
+        auth.password,
+        auth.orcid,
+        auth.affilation
+
+    FROM article   AS a
+    JOIN publication AS p ON a.publication_id = p.id
+    JOIN author      AS auth ON p.author_id      = auth.id
+    ORDER BY p.publication_date DESC;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `sp_book_list` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_book_list`()
+BEGIN
+    SELECT
+        b.isbn,
+        b.genre,
+        b.edition,
+        b.publication_id,
+
+        p.description,
+        p.publication_date,
+        p.title,
+
+        
+        a.id         AS id,
+        a.first_name,
+        a.last_name,
+        a.username,
+        a.email,
+        a.password,
+        a.orcid,
+        a.affilation
+
+    FROM book       AS b
+    JOIN publication AS p ON b.publication_id = p.id
+    JOIN author      AS a ON p.author_id      = a.id
+
+    ORDER BY p.publication_date DESC;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `sp_create_book` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_create_book`(
+    IN p_title VARCHAR(100),
+    IN p_description VARCHAR(100),
+    IN p_date DATE,
+    IN p_author_id INT,
+    IN p_isbn VARCHAR(20),
+    IN p_genre VARCHAR(50),
+    IN p_edition INT
+)
+BEGIN
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        ROLLBACK;
+    END;
+
+    START TRANSACTION;
+
+    INSERT INTO publication (
+        title,
+        description,
+        publication_date,
+        author_id,
+        type
+    ) VALUES (
+        p_title,
+        p_description,
+        p_date,
+        p_author_id,
+        'book'
+    );
+
+    SET @new_pub_id = LAST_INSERT_ID();
+
+    INSERT INTO book (
+        publication_id,
+        isbn,
+        genre,
+        edition
+    ) VALUES (
+        @new_pub_id,
+        p_isbn,
+        p_genre,
+        p_edition
+    );
+
+    COMMIT;
+
+    SELECT @new_pub_id AS pub_id;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `sp_delete_book` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_delete_book`(IN p_id INT)
+BEGIN
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        ROLLBACK;
+    END;
+
+    START TRANSACTION;
+
+    DELETE FROM book        WHERE publication_id = p_id;
+    DELETE FROM publication WHERE id             = p_id;
+
+    COMMIT;
+
+    SELECT 1 AS OK;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `sp_find_book` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_find_book`(IN p_id INT)
+BEGIN
+    SELECT
+        b.isbn,
+        b.genre,
+        b.edition,
+        b.publication_id,
+        p.description,
+        p.publication_date,
+        p.title,
+        p.author_id,
+        a.first_name,
+        a.last_name
+    FROM book AS b
+    JOIN publication AS p ON b.publication_id = p.id
+    JOIN author AS a      ON p.author_id       = a.id
+    WHERE b.publication_id = p_id
+    ORDER BY p.publication_date DESC;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-08-05 19:25:07
